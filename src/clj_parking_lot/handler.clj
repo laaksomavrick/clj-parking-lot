d7d5e84f961b4de5ccbd90d918109e95ca175985
mlaakso@dev <laakso.mavrick@gmail.com>
2019-02-08 01:29:51 -0500

    Ticket schema expanded; capacity check; find ticket

--- src/clj_parking_lot/handler.clj
+++ src/clj_parking_lot/handler.clj
@@ -1,29 +1,57 @@
 (ns clj-parking-lot.handler
   (:require [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [compojure.api.exception :as ex]))
 
 (s/defschema Ticket
-  {:ticket {:id s/Int}})
+  {:ticket {:id s/Int :paid s/Bool :owing s/Int :rate s/Int}})
 
-(defonce tickets (atom (hash-map)))
+(s/defschema TicketInput
+  {:rate s/Int})
 
-(defn create-ticket! []
+(def parking-lot-capacity 24)
+(def tickets (atom (hash-map)))
+(def rates {0 300  ;; 1 hr
+            1 450  ;; 3 hr
+            2 600  ;; 6 hr
+            3 750  ;; all day
+            })
+
+(defn below-capacity?
+  []
+  (< (count @tickets) parking-lot-capacity))
+
+;; todo handle invalid rate
+(defn get-owing
+  [rate]
+  (get rates rate))
+
+(defn create-ticket!
+  [ticket-input]
   (let [id (inc (count @tickets))
-        ticket {:id id}]
+        owing (get-owing (get ticket-input :rate))
+        ticket (merge ticket-input {:id id :paid false :owing owing})]
     (swap! tickets assoc id ticket)
     ticket))
 
 (def app
   (api
-   {:swagger
-    {:ui "/"
-     :spec "/swagger.json"
-     :data {:info {:title "clj-parking-lot"
-                   :description "Compojure Api example"}
-            :tags [{:name "api", :description "some apis"}]}}}
 
    (POST "/customers" []
+     :body [ticket-input TicketInput]
      :return Ticket
-     :summary "Issues a new ticket for the requesting client"
-     (ok {:ticket (create-ticket!)}))))
+     ;; use cond to check that rate is valid here
+     (if (below-capacity?)
+       (ok {:ticket (create-ticket! ticket-input)})
+       (bad-request {:error "The parking lot is full!"})))
+
+   (GET "/tickets/:id" []
+     :return Ticket
+     :path-params [id :- Long]
+     (if-let [ticket (@tickets id)]
+       (ok {:ticket ticket})
+       (not-found {:error "Ticket not found"})))
+
+   (GET "/tickets" []
+     (ok {:tickets @tickets}))))
